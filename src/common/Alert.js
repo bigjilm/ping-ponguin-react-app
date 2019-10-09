04a8e205da6aa8c44622fb51f2f7980a94e15643
bigjilm <patera@posteo.de>
2019-10-09 16:39:13 +0200

    Add Alert component

---
+++ src/common/Alert.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+
+Alert.propTypes = {
+  target: PropTypes.string,
+  children: PropTypes.node,
+}
+
+export default function Alert({ target, children }) {
+  return <AlertStyled>{target ? setAlert(target) : children}</AlertStyled>
+
+  function setAlert(target) {
+    const alertName = {
+      name: 'Bitte gib einen Namen ein',
+      residence: 'Bitte gib einen Wohnort ein',
+      abilityLeft: 'Bitte gib an, wie gut du mit links spielst',
+      abilityRight: 'Bitte gib an, wie gut du mit rechts spielst',
+    }
+    return alertName[target]
+  }
+}
+
+const AlertStyled = styled.span`
+  font-size: 14px;
+  color: #c8232a;
+`
