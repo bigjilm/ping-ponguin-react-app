89b3d21b17c7218e99f98c7a470149c99985521d
bigjilm <patera@posteo.de>
2019-10-10 13:26:49 +0200

    Add UserSession model

---
+++ server/models/UserSession.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSessionSchema = new mongoose.Schema({
+  userId: {
+    type: Number,
+    default: -1,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now(),
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const UserSession = mongoose.model('UserSession', userSessionSchema)
+
+module.exports = UserSession
