5efed331286f254b87a304b4641cecc1c90eece4
bigjilm <patera@posteo.de>
2019-10-13 11:36:40 +0200

    Change get request to not get email and password

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,18 @@ const User = require('../models/User')
 
 router.get('/', (req, res) => {
   User.find()
-    .then(users => res.json(users))
+    .then(users => {
+      const usersData = users.map(user => ({
+        _id: user._id,
+        name: user.name,
+        residence: user.residence,
+        abilityLeft: user.abilityLeft,
+        abilityRight: user.abilityRight,
+        imageURL: user.imageURL,
+        isDeleted: user.isDeleted,
+      }))
+      res.json(usersData)
+    })
     .catch(err => res.status(404).json(err))
 })
 
