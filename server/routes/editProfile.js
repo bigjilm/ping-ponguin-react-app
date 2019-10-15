739448302e145bebb72d867b57db3131ca30d1b1
bigjilm <patera@posteo.de>
2019-10-15 17:37:09 +0200

    Update editProfile method in backend

--- server/routes/editProfile.js
+++ server/routes/editProfile.js
@@ -1,25 +1,29 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
-const UserSession = require('../models/UserSession')
 
 router.patch('/', (req, res) => {
   const { token } = req.query
   const { password } = req.body
-  UserSession.findById(token).then(session => {
-    const userId = session.userId
-    User.findByIdAndUpdate(userId, {
-      ...req.body,
-      password: generateHash(password),
-    })
-      .then(() => {
-        res.json({
-          success: true,
-          message: 'Your profile has been edited',
-        })
-      })
-      .catch(err => res.status(400).json(err))
+  let encryptedPassword
+
+  if (password) {
+    encryptedPassword = generateHash(password)
+  } else {
+    encryptedPassword = ''
+  }
+
+  User.findByIdAndUpdate(token, {
+    ...req.body,
+    password: encryptedPassword,
   })
+    .then(() => {
+      res.json({
+        success: true,
+        message: 'Your profile has been edited',
+      })
+    })
+    .catch(err => res.status(400).json(err))
 })
 
 function generateHash(password) {
