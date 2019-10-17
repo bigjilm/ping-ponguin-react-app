efa9cb1c319769a778ebc186f2e7ecb9270275b7
bigjilm <patera@posteo.de>
2019-10-17 10:22:54 +0200

    Update patch method in backend with Model.save instead of Model.update

--- server/routes/editProfile.js
+++ server/routes/editProfile.js
@@ -4,7 +4,15 @@ const User = require('../models/User')
 
 router.patch('/', (req, res) => {
   const { id } = req.query
-  const { password } = req.body
+  const {
+    name,
+    residence,
+    abilityLeft,
+    abilityRight,
+    imageURL,
+    email,
+    password,
+  } = req.body
   let encryptedPassword
 
   if (password) {
@@ -13,17 +21,49 @@ router.patch('/', (req, res) => {
     encryptedPassword = ''
   }
 
-  User.findByIdAndUpdate(id, {
-    ...req.body,
-    password: encryptedPassword,
-  })
+  //Doppelte Email ausschließen
+
+  User.findById(id)
     .then(user => {
-      res.json({
-        success: true,
-        message: 'Your profile has been edited',
-      })
+      // name && (user.name = name)
+      // residence && (user.residence = residence)
+      // abilityLeft && (user.abilityLeft = abilityLeft)
+      // abilityRight && (user.abilityRight = abilityRight)
+      // imageURL && (user.imageURL = imageURL)
+      // email && (user.email = email)
+      // password && (user.password = password)
+
+      user.name = name
+      user.residence = residence
+      user.abilityLeft = abilityLeft
+      user.abilityRight = abilityRight
+      user.imageURL = imageURL
+      user.email = email
+      user.password = encryptedPassword
+
+      user
+        .save()
+        .then(() => {
+          res.json({
+            success: true,
+            message: 'Your profile has been edited',
+          })
+        })
+        .catch(err => res.status(400).json(err))
     })
     .catch(err => res.status(400).json(err))
+
+  // User.findByIdAndUpdate(id, {
+  //   ...req.body,
+  //   password: encryptedPassword,
+  // })
+  //   .then(() => {
+  //     res.json({
+  //       success: true,
+  //       message: 'Your profile has been edited',
+  //     })
+  //   })
+  //   .catch(err => res.status(400).json(err))
 })
 
 function generateHash(password) {
