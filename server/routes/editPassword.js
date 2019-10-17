d23b1ec103dc487227d8644bc7eb6dbd09eb4173
bigjilm <patera@posteo.de>
2019-10-17 11:30:23 +0200

    Add editPassword route on server

---
+++ server/routes/editPassword.js
@@ -0,0 +1,51 @@
+const router = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+
+router.patch('/', (req, res) => {
+  const { id } = req.query
+  const { oldPassword, newPassword, newPasswordRepeat } = req.body
+
+  let newPasswordEncrypted
+
+  if (!(newPassword === newPasswordRepeat)) {
+    res.json({
+      success: false,
+      message: 'The two new passwords are not the same',
+    })
+  } else {
+    if (newPassword) {
+      newPasswordEncrypted = generateHash(newPassword)
+    } else {
+      newPasswordEncrypted = ''
+    }
+  }
+
+  User.findById(id)
+    .then(user => {
+      if (!user.validPassword(oldPassword)) {
+        res.json({
+          success: false,
+          message: 'The old password is wrong',
+        })
+      } else {
+        user.password = newPasswordEncrypted
+        user
+          .save()
+          .then(
+            res.json({
+              success: true,
+              message: 'Your new password has been saved',
+            })
+          )
+          .catch(err => res.status(400).json(err))
+      }
+    })
+    .catch(err => res.status(400).json(err))
+})
+
+function generateHash(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+module.exports = router
