cbb5624ceab4a63da1603b58a3766138f0198acd
bigjilm <patera@posteo.de>
2019-10-28 16:45:15 +0100

    Set timeout for later deletion

--- server/routes/deleteUser.js
+++ server/routes/deleteUser.js
@@ -3,14 +3,25 @@ const User = require('../models/User')
 
 router.delete('/', (req, res) => {
   const { id } = req.query
-  User.findByIdAndDelete(id)
-    .then(user =>
-      res.json({
+
+  User.findByIdAndUpdate(id, { isDeleted: true }, { new: true })
+    .then(user => {
+      setTimeout(() => user.remove(), 15000)
+      return res.json({
         success: true,
-        message: 'User ' + user.name + ' deleted',
+        message: 'User ' + user.name + ' marked for deletion',
       })
-    )
+    })
     .catch(err => res.json(err))
+
+  //   User.findByIdAndDelete(id)
+  //     .then(user =>
+  //       res.json({
+  //         success: true,
+  //         message: 'User ' + user.name + ' deleted',
+  //       })
+  //     )
+  //     .catch(err => res.json(err))
 })
 
 module.exports = router
